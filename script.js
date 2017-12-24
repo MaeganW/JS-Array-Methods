9ccc04d86de2aa79dec110ba6a324be77a292b67
Maegan Womble <maeganwomble@gmail.com>
2017-12-24 14:10:01 -0600

    html markup and arrays

---
+++ script.js
@@ -0,0 +1,9 @@
+const philosophers = ["Theodor Adorno", "Thomas Aquinas", "Hannah Arendt", "Aristotle", "Augustine", "Francis Bacon", "Roland Barthes", "Georges Bataille", "Jean Baudrillard", "Simone de Beauvoir", "Walter Benjamin", "George Berkeley"];
+const pets = [
+    {name: "Buttercup", animal: "dog", color: "brown", age: 3},
+    {name: "Daisy", animal: "dog", color: "yellow", age: 10},
+    {name: "Boots", animal: "cat", color: "brown", age: 5},
+    {name: "Patches", animal: "cat", color: "orange", age: 2},
+    {name: "Talcum", animal: "sheep", color: "white", age: 6},
+    {name: "Chip", animal: "hen", color: "brown", age: 2},
+];
